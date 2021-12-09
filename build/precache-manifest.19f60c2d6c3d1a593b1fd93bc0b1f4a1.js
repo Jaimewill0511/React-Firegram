13825eb9997d30a049544096c11c3eccfef6a214
William Eyimofe Binitie <67603735+Jaimewill0511@users.noreply.github.com>
2021-12-09 20:28:32 +0100

    Add files via upload

---
+++ build/precache-manifest.19f60c2d6c3d1a593b1fd93bc0b1f4a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08c93b2e5655d51f38f1a31dc2b1807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f2f47d1f286d6201b57",
+    "url": "/static/css/main.f370ba14.chunk.css"
+  },
+  {
+    "revision": "adf456938225afd1ef86",
+    "url": "/static/js/2.ab3813b8.chunk.js"
+  },
+  {
+    "revision": "924dbda79911d900c084933f9bb002ad",
+    "url": "/static/js/2.ab3813b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2f47d1f286d6201b57",
+    "url": "/static/js/main.0259da70.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
